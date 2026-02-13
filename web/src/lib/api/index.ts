d2f4ea99bb4ad17bb52da23608c07e55150f78b8
Himanshu Thakur <himanshu.t.dev@gmail.com>
2026-02-13 12:31:52 +0530

    feat: re-add api export alias for httpClient

--- web/src/lib/api/index.ts
+++ web/src/lib/api/index.ts
@@ -1,3 +1,6 @@
 export { httpClient } from "./client";
 export { authApi } from "./auth.api";
 export * from "./types";
+
+// Export httpClient as 'api' for convenience
+export { httpClient as api } from "./client";
